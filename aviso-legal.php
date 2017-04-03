54b12b97fe569f366948fd0393fa45caa8259d31
mperez <mpf@openmailbox.org>
2017-04-03 04:25:33 +0200

    Moved the information of the entity from the translation part (locale) to the configuration (vistas/config.html)

--- aviso-legal.php
+++ aviso-legal.php
@@ -1,5 +1,5 @@
 <?php
 include_once "lib/functions.php";
 $template = $twig->loadTemplate('aviso-legal.html');
-$datos = array('user'=>autentificado());
+$datos = array('user'=>autentificado(),'lang'=>$lang);
 echo $template->render($datos);
