7a174b3ce963df8d6b3557895996ca0c169ce77d
mperez <mpf@openmailbox.org>
2017-04-06 19:29:52 +0200

    the test of the existence of the email is done using UPPER

--- account_activate.php
+++ account_activate.php
@@ -16,7 +16,7 @@ if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !
     $conn = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASS);
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     $muser = array('email'=>$email, 'hash'=>$hash);
-    $result=$conn->prepare( "SELECT count(*) as nemail FROM users_can_participate WHERE email=:email AND hash=:hash;");
+    $result=$conn->prepare( "SELECT count(*) as nemail FROM users_can_participate WHERE UPPER(email)=UPPER(:email) AND hash=:hash;");
     $result->execute($muser);
     foreach ($result as $cont) {
     	$nemail = $cont['nemail'];
@@ -32,7 +32,7 @@ if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !
 		$conn = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASS);
 		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 		// Select from users_can_participate
-		$result=$conn->prepare( "select nombre, apellidos,email, password from users_can_participate WHERE email=:email;");
+		$result=$conn->prepare( "select nombre, apellidos,email, password from users_can_participate WHERE UPPER(email)=UPPER(:email);");
         $muser = array('email'=>$email);
 		$result->execute($muser);
         list($nombre, $apellidos, $email, $password)=$result->fetch(PDO::FETCH_NUM);
@@ -42,14 +42,14 @@ if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !
 		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         $muser = array('nombre'=>$nombre,'apellidos'=>$apellidos,'email'=>$email,'pass'=>$password);
         $result=$conn->prepare( "INSERT INTO users(nombre, apellidos, email, password) 
-            VALUES(:nombre, :apellidos, :email, :pass);");
+            VALUES(:nombre, :apellidos, LOWER(:email), :pass);");
 		$result->execute($muser);
 
         // DELETE user from users_can_participate
 		$conn = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASS);
 		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 		$muser = array('email'=>$email);
-		$result=$conn->prepare( "delete from users_can_participate WHERE email=:email;");
+		$result=$conn->prepare( "delete from users_can_participate WHERE UPPER(email)=UPPER(:email);");
 		$result->execute($muser);
         
       } 
